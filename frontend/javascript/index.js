4353cbc9231c787b1250f3e00e1c3fd3125813f6
Westley Winks <westleywinks@protonmail.com>
2024-10-02 15:43:21 +0100

    add cv page

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,7 @@
 import "$styles/index.scss"
-import "$styles/syntax-highlighting.css"
+
+// import styles from "../**/*.{scss,css}"
+// Object.entries(styles).forEach(style => style)
 
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
