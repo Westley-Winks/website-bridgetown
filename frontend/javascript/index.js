298e0d2aa897926639d255a4853137172a6ffbb1
Westley Winks <westleywinks@protonmail.com>
2024-10-07 16:25:04 +0100

    style table of contents
    - Restructured article containers
    - Added floating table of contents button

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -5,3 +5,15 @@ import "$styles/index.scss"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
+
+/* TOC button */
+(function () {
+  var button = document.getElementById("menu-button");
+  if (button) {
+    var menu = document.getElementById("tableOfContents");
+    button.addEventListener("click", function () {
+      var expanded = this.getAttribute("aria-expanded") === "true";
+      this.setAttribute("aria-expanded", !expanded);
+    });
+  }
+})();
--- plugins/builders/inspectors.rb
+++ plugins/builders/inspectors.rb
@@ -3,11 +3,15 @@ class Builders::Inspectors < SiteBuilder
     inspect_html do |document|
       toc = document.query_selector "article ul#markdown-toc"
       if toc
-        main = document.query_selector "main"
         title = document.query_selector "h1"
         article = document.query_selector "article"
+
         article.add_previous_sibling toc
-        toc.wrap("<nav class='toc'></nav>")
+        toc.wrap("<div class='toc-container'></div>")
+        toc.wrap("<nav class='toc card'></nav>")
+        toc.prepend_child "<a href='#top'>#{title.content}</a>"
+        nav = document.query_selector "main nav"
+        nav.add_previous_sibling "<button id='menu-button' aria-expanded='false'>Table Of Contents</button>"
       end
     end
   end
